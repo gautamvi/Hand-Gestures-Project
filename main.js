e8432769bbd684a63abcd9f0e43b9847359ad0aa
gautamvi <85584126+gautamvi@users.noreply.github.com>
2022-02-07 20:06:05 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -29,3 +29,46 @@ Webcam.set({
         var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
         synth.speak(utterThis);
     }
+
+    function check(){
+        img = document.getElementById("captured_image");
+        classifier.classify(img , gotResult);
+    }
+    
+    function gotResult(error ,results){
+        if(error){
+            console.error(error);
+        } else {
+            console.log(results);
+            document.getElementById("result_gesture_name").innerHTML = results[0].label;
+            document.getElementById("result_gesture_name2").innerHTML = results[1].label;
+            prediction_1 = results[0].label;
+            prediction_2 = results[1].label;
+            speak();
+            if(results[0].label == "Peace"){
+                document.getElementById("update_gesture").innerHTML = "&#9996;";
+            }
+            if(results[0].label == "Thumbs Down"){
+                document.getElementById("update_gesture").innerHTML = "&#128078;";
+            }
+            if(results[0].label == "Thumbs Up"){
+                document.getElementById("update_gesture").innerHTML = "&#128077;";
+            }
+            if(results[0].label == "Okay"){
+                document.getElementById("update_gesture").innerHTML = "&#128076;";
+            }
+
+            if(results[1].label == "Peace"){
+                document.getElementById("update_gesture2").innerHTML = "&#9996;";
+            }
+            if(results[1].label == "Thumbs Down"){
+                document.getElementById("update_gesture2").innerHTML = "&#128078;";
+            }
+            if(results[1].label == "Thumbs Up"){
+                document.getElementById("update_gesture2").innerHTML = "&#128077;";
+            }
+            if(results[1].label == "Okay"){
+                document.getElementById("update_gesture2").innerHTML = "&#128076;";
+            }
+        }
+    }
